cb50e941f13d25ec78c58790301e08a32acea9d8
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2016-04-02 15:38:59 +0200

    cross product sin

--- computational-geometry/SortByPolarAngleTest.java
+++ computational-geometry/SortByPolarAngleTest.java
@@ -1,7 +1,6 @@
 import java.util.*;
 import static org.junit.Assert.*;
 import org.junit.Test;
-import javafx.geometry.Point2D;
 
 /**
   * @author Mighty Cohadar 
@@ -10,55 +9,54 @@ public class SortByPolarAngleTest {
 
 	public static Random random = new Random();
 	
-	public static void shuffle(Point2D[] A) {
-		for (int i = 0; i < A.length-1; i++) {
-			int j = i + random.nextInt(A.length - i);
-			Point2D temp = A[i];
-			A[i] = A[j];
-			A[j] = temp;
-		}
-	}
-
-	public static double crossProduct(Point2D p1, Point2D p2) {
-		return p1.getX() * p2.getY() - p1.getY() * p2.getX();
-	}		
-
 	@Test
 	public void test() {
-		Point2D pr = new Point2D(4, -3); // reference point must be outside bounding rectangle
-		Point2D p0 = new Point2D(1, 0);
-		Point2D p1 = new Point2D(1, 1);
-		Point2D p2 = new Point2D(0, 1);
-		Point2D p3 = new Point2D(-1, 1);
-		Point2D p4 = new Point2D(-1, 0);
-		Point2D p5 = new Point2D(-1, -1);
-		Point2D p6 = new Point2D(0, -1);
-		Point2D p7 = new Point2D(1, -1);
-		Point2D[] P = new Point2D[] {p0, p1, p2, p3, p4, p5, p6, p7};
-		shuffle(P);
-		Point2D[] Q = Arrays.copyOf(P, P.length);
+		Point[] P = new Point[100];
+		for (int i = 0; i < P.length; i++) {
+			P[i] = randomPointFQ();
+		}
+		Point[] Q = Arrays.copyOf(P, P.length);
 		// sort P by angle
-		Arrays.sort(P, new Comparator<Point2D> () {
-			public int compare(Point2D a, Point2D b) {
-				Point2D c = a.subtract(pr);
-				Point2D d = b.subtract(pr);
-				double angleA = Math.atan2(c.getY(), c.getX());
-				double angleB = Math.atan2(d.getY(), d.getX());
-				return Double.compare(angleA, angleB);
+		Arrays.sort(P, new Comparator<Point> () {
+			public int compare(Point a, Point b) {
+				double angleA = Math.atan2(a.y, a.x);
+				double angleB = Math.atan2(b.y, b.x);
+				return Double.compare(angleB, angleA);
 			}
 		});
 		// sort Q by crossProduct
-		Arrays.sort(Q, new Comparator<Point2D> () {
-			public int compare(Point2D a, Point2D b) {
-				// positive crossProduct is negative angle, hence (b, a)
-				return (int)Math.signum(crossProduct(b.subtract(pr), a.subtract(pr)));
+		Arrays.sort(Q, new Comparator<Point> () {
+			public int compare(Point a, Point b) {
+				return (int)Math.signum(a.crossProduct(b));
 			}
 		});		
 		assertArrayEquals(P, Q);
 	}
 
+	/**
+	 * Sign of angle difference and crossProduct match as long as all points belong to the Quadrant pairs (1,2) or (1,4).
+	 * That is as long either all X have the same sign or all Y have the same sign.
+	 */
+	@Test
+	public void testFirstQuadrant() {
+		for (int i = 0; i < 100; i++) {
+			Point p1 = randomPointFQ();
+			Point p2 = randomPointFQ();
+			double angle1 = Math.atan2(p1.y, p1.x);
+			double angle2 = Math.atan2(p2.y, p2.x);
+			double cross = p1.crossProduct(p2);
+			double dot = p1.dotProduct(p2);
+			debug(Math.signum(angle2 - angle1), Math.signum(cross));
+			assertEquals(Math.signum(angle2 - angle1), Math.signum(cross), 1e-6);
+		}
+	}
+
+	public static Point randomPointFQ() {
+		return new Point(Math.random() * 1000 - 500, Math.random() * 1000);
+	}
+
 	static void debug(Object...os) {
-		System.err.printf("%.65536s\n", Arrays.deepToString(os));
+		// System.err.printf("%.65536s\n", Arrays.deepToString(os));
 	}
 
 }
