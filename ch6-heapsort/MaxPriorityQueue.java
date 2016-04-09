baf06ef10aa3d9f012150feb023c87e93bdc3c03
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2016-04-09 18:42:24 +0200

    priority queue no swap

--- ch6-heapsort/MaxPriorityQueue.java
+++ ch6-heapsort/MaxPriorityQueue.java
@@ -9,20 +9,20 @@ public class MaxPriorityQueue<E> {
 	private int n = 0;
 	private final int capacity;
 	private final Comparator<E> comparator;
-	private List<E> heap;
+	private List<E> list;
 
 	public MaxPriorityQueue(int capacity, Comparator<E> comparator) {
 		this.capacity = capacity;
 		this.comparator = comparator;
-		this.heap = new ArrayList<>();
-		this.heap.add(null); // heap starts from 1
+		this.list = new ArrayList<>();
+		this.list.add(null); // list starts from 1
 	}
 
 	public boolean add(E e) {
 		if (e == null) {
 			throw new NullPointerException("cannot insert null elements");
 		}
-		if (heap.add(e)) {
+		if (list.add(e)) {
 			n++;
 			floatUp(n);
 			return true;
@@ -34,22 +34,22 @@ public class MaxPriorityQueue<E> {
 		if (n == 0) {
 			return null;
 		}
-		return heap.get(1);
+		return list.get(1);
 	} 
 
 	public E poll() {
 		if (n == 0) {
 			return null;
 		}
-		E ret = heap.get(1);
-		swap(1, n--);
+		E ret = list.get(1);
+		list.set(1, list.get(n--));
 		sinkDown(1);
 		return ret;
 	} 
 
 	// this is the method that java's PriorityQueue lacks.
 	public E set(int index, E e) {
-		E ret = heap.get(index);
+		E ret = list.get(index);
 		int comp = comparator.compare(e, ret);
 		if (comp < 0) {
 			sinkDown(index);
@@ -64,40 +64,39 @@ public class MaxPriorityQueue<E> {
 	}
 
 	private void sinkDown(int p) {
+		E t = list.get(p);
 		while (p <= n / 2) {
 			int l = 2 * p;
 			int r = l + 1;
-			int imax = p;
-			if (l <= n && comparator.compare(heap.get(imax), heap.get(l)) < 0) {
-				imax = l;
+			int c = -1;
+			if (l <= n && comparator.compare(t, list.get(l)) < 0) {
+				c = l;
 			}
-			if (r <= n && comparator.compare(heap.get(imax), heap.get(r)) < 0) {
-				imax = r;
+			if (r <= n && comparator.compare((c == -1)?t:list.get(c), list.get(r)) < 0) {
+				c = r;
 			}			
-			if (imax == p) {
+			if (c == -1) {
 				break;
 			} else {
-				swap(p, imax);
-				p = imax;
+				list.set(p, list.get(c));
+				p = c;
 			}
 		}
+		list.set(p, t);
 	}
 
 	private void floatUp(int c) {
+		E t = list.get(c);
 		while (c > 1) {
 			int p = c / 2;
-			if (comparator.compare(heap.get(p), heap.get(c)) < 0) {
-				swap(p, c);
+			if (comparator.compare(list.get(p), t) < 0) {
+				list.set(c, list.get(p));
 				c = p;
 			} else {
 				break;
 			}
 		}
+		list.set(c, t);
 	}
 
-	private void swap(int i, int j) {
-		E t = heap.get(i);
-		heap.set(i, heap.get(j));
-		heap.set(j, t);
-	}
 }
