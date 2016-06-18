181aeaf50128538438fa5236e17c7a9cd22f65e9
cohadar <cohadar@gmail.com>
2016-06-18 11:59:10 +0200

    bstest

--- ch02-getting-started/BinarySearchTest.java
+++ ch02-getting-started/BinarySearchTest.java
@@ -7,6 +7,20 @@ import org.junit.Test;
   */
 public class BinarySearchTest {
 
+	static int binarySearch(int[] A, int li, int re, int val) {
+		while (li < re) {
+			int mi = (li + re) >>> 1;
+			if (A[mi] > val) {
+				re = mi;
+			} else if (A[mi] < val) {
+				li = mi + 1;
+			} else {
+				return mi;
+			}
+		}
+		return ~li;
+	}
+
 	@Test
 	public void testFullRange() {
 		int[] A = randomArray(1000, -1000, 1000);
@@ -14,7 +28,7 @@ public class BinarySearchTest {
 		for (int i = 0; i < 100000; i++) {
 			int v = -1111 + random.nextInt(2222);
 			int a = Arrays.binarySearch(A, v);
-			int b = BinarySearch.binarySearch(A, v, 0, A.length - 1);
+			int b = binarySearch(A, 0, A.length, v);
 			if (a < 0 && b < 0) {
 				assert a == b;
 				continue;
@@ -31,10 +45,10 @@ public class BinarySearchTest {
 			int v = -1111 + random.nextInt(2222);
 			int t1 = random.nextInt(A.length);
 			int t2 = random.nextInt(A.length);
-			int left = Math.min(t1, t2);
-			int right = Math.max(t1, t2);
-			int a = Arrays.binarySearch(A, left, right+1, v);
-			int b = BinarySearch.binarySearch(A, v, left, right);
+			int li = Math.min(t1, t2);
+			int re = Math.max(t1, t2) + 1;
+			int a = Arrays.binarySearch(A, li, re, v);
+			int b = binarySearch(A, li, re, v);
 			if (a < 0 && b < 0) {
 				assert a == b;
 				continue;
